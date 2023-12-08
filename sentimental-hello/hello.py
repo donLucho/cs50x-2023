209536932405b094f8799fe6cb71285f2fd7c579
Luis Artavia <luva.arso@outlook.com>
2023-12-08 11:36:18 -0600

    (Wed, Sep 6, 2023, 7:18 PM -06:00)

---
+++ sentimental-hello/hello.py
@@ -0,0 +1,11 @@
+# Required Problem set 6 by Luis Artavia - sentimental-hello
+# draft processed on: September 06, 2023
+# submitted for grade on: September 06, 2023
+
+
+def main():
+    name = input("What is your name?\n")
+    print(f"hello, {name}")
+
+
+main()
